f374d3ec59fd3252e2e0c0a61d89319f837f918d
dtelega <dtelega@MEGATHRON>
2018-01-08 18:05:20 +0200

    add front, fix d<0. show result, and some details

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// TODO: polynomial degree
-// TODO: sqrt
-
 function calculate() {
 
-    var a = 0;
-    var b = 0;
-    var c = 0;
+    var a=0, b=0, c=0;
+    var  result_field = document.getElementById('result_field');
+    var details_field = document.getElementById('details_field');
+    var result, details;
+
     var equation = document.getElementById('equation').value;
 
     if (equation.search('=') === -1 || equation.search('X') === -1){
@@ -42,29 +41,47 @@ function calculate() {
             c += Number(equation[i]);
 
         }
-        else {
+        else if (!(equation[i].indexOf("0") + 1)){
             alert("The polynomial degree is stricly greater than 2, I can't solve.");
         }
     });
-
+    details = a+"*X<sup>2</sup> + "+b+"*X + "+c+" = 0;<br>";
     console.log(a,b,c);
     var D = (b*b) - (4*a*c);
-
+    details += "D="+D+";\n<br>";
     console.log("D="+D);
     var x1,x2;
-    if (D < 0)
-        console.log("nety x")
+    if (D < 0) {
+
+        details += "пара комплексно-сопряженных корней<br>";
+        result = "x<sub>1</sub> = (";
+        result += - b / ( 2 * a );
+        result += ", ";
+        result += (-D)**(0.5) / ( 2 * a );
+        result += "), x<sub>2</sub> = (";
+        result += - b / ( 2 * a);
+        result += ", ";
+        result += -((-D)**(0.5) / ( 2 * a ));
+        result += ").";
+
+    }
     else if (D > 0)
     {
-        x1 = (-b - Math.sqrt(D)) / (2*a); // TODO: my sqrt
-        x2 = (-b + Math.sqrt(D)) / (2*a);
+        //x1 = (-b - Math.sqrt(D)) / (2*a);
+        //x2 = (-b + Math.sqrt(D)) / (2*a);
+        x1 = (-b - D**(0.5)) / (2*a);
+        x2 = (-b + D**(0.5)) / (2*a);
+        result = "X<sub>1</sub> = "+x1+"<br>X<sub>2</sub> = "+x2+"";
         console.log("x1="+x1+";x2="+x2);
     }
-    else if (D == 0)
+    else if (D === 0)
     {
         x1 = (-b)/(2*a);
         console.log("x="+x1);
+        result = "x="+x1+";\n";
     }
+    result_field.innerHTML = result;
+    details_field.innerHTML = details;
 }
 
 function check_input(input) {
